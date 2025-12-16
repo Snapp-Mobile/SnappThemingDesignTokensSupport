58f06be497dcc48f3f1b2aaf188e73dc0138c086
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-12-16 12:26:45 +0200

    Document FontWeightMapping

--- Sources/SnappThemingDesignTokensSupport/FontWeightMapping.swift
+++ Sources/SnappThemingDesignTokensSupport/FontWeightMapping.swift
@@ -7,4 +7,11 @@
 
 import SnappDesignTokens
 
+/// Type alias for a dictionary that maps ``SnappDesignTokens/FontWeightValue/RawValue`` (which represents a font weight from design tokens)
+/// to a `String` that typically represents a platform-specific font weight name or a numerical value.
+///
+/// This mapping is crucial for apply design token font weights to the ``SnappTheming/SnappThemingFontInformation`` of the
+/// ``SnappDesignTokens/FontFamilyValue`` when converting ``SnappDesignTokens/TypographyValue`` into
+/// ``SnappTheming/SnappThemingTypographyRepresentation``.
+/// It is used in `DesignTokensTypographyValueExtractor.swift` to construct `postScriptName` font names.
 public typealias FontWeightMapping = [FontWeightValue.RawValue: String]
