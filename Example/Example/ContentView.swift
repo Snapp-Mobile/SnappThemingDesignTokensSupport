a8bb04734ed61b5a5ee774d1f7969092386d13de
Voiko Volodymyr <vojkovladimir@gmail.com>
2025-12-19 15:01:57 +0200

    Add empty Example project

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Volodymyr Voiko on 19.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
